aa857e0ada015a02fcc7237269769232b691b4b4
Duongnhat512 <duongnhat512@gmail.com>
2023-05-11 07:18:50 +0700

    ..

--- js/validate.js
+++ js/validate.js
@@ -10,11 +10,11 @@ $(document).ready(function(){
              
             // 1 username
             
-            let regUserName = /^[a-zA-Z0-9]+$/;
+            let regUserName = /^[a-zA-Z][a-zA-Z0-9]+$/;
         
             var username = getValue('username');
             if (!regUserName.test(username)){
-                alert('Tên tài khoản không được chứa ký tự đặc biệt.');
+                alert('Tên tài khoản không được chứa ký tự đặc biệt và bắt đầu bằng ký tự chữ.');
                 return false;
             }
         
@@ -29,24 +29,32 @@ $(document).ready(function(){
         
             var password = getValue('password');
             if (!regPassword.test(password)){
-                alert('Mật khẩu phải bắt đầu bằng chữ in hoa và không chứa ký tự.');
+                alert('Mật khẩu phải bắt đầu bằng chữ in hoa và không chứa ký tự đặc biệt.');
                 return false;
             }
             if(password.trim().length < 5){
                 alert('Mật khẩu phải từ 5 ký tự trở lên.');
                 return false;
             }
+            //3. Email
+
+            let regEmail = /^[a-zA-Z][a-zA-Z_0-9]+\@gmail\.(com|vn)$/;
+            var email = document.querySelector('#Email').value;
+
+            if(!regEmail.test(email)){
+                alert('Email không nhập vào không chính xác.');
+                return false;
+            }
         
-            // 3. phone
+            // 4. phone
         
-            let regPhone = /^0[1-9][0-9]{8}$/;
+            let regPhone = /^0[2-9][0-9]{8}$/;
             var phone = getValue('phone');
         
             if(!regPhone.test(phone)){
                 alert('Số điện thoại không tồn tại.');
                 return false;
             }
-
 
         
             return true;
